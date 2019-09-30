03b171591197041bf01999bdc2ce24816370c202
Ítalo Gomes Vieira <italogomes@A102-27L.imd.local>
2019-09-30 19:59:15 -0300

    Update ex1

--- ex1.asm
+++ ex1.asm
@@ -4,21 +4,29 @@ var2:	.word 1,5,9,3,1
 	.text
 	.globl main
 	
-main: 
+main:
+	li $t1, 1
+	li $s3, 5
+	li $s4, 7
+
 	beq $t1, 0, L0
 	beq $t1, 1, L1
 	beq $t1, 2, L2
 	beq $t1, 3, L3
 
 	L0:
-		add $t1, $s0, $s1 # f = i + j
+		add $s0, $s1, $s2   # f = i + j
+		b END
 	L1:
-		add $t1, $s2, $s3   # f = g + h
+		add $s0, $s3, $s4   # f = g + h
+		b END
 	L2:
-		sub $t1, $s2, $s3   # f = g - h
+		sub $s0, $s3, $s4   # f = g - h
+		b END
 	L3:
-		sub $t1, $s0, $s3   # f = i - h
-		
-	li $v0, 1
-	addi $s0, $t1, 0
-	syscall 
+		sub $s0, $s1, $s4   # f = i - h
+	
+	END:
+		li $v0, 1
+		addi $a0, $s0, 0
+		syscall 
